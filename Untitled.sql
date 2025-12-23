df8adba22cf10bfb412426d71e8615b831aa9360
BerendInergy1 <bwijte@ilionx.com>
2025-12-23 08:33:42 +0000

    delete materializations

---
+++ Untitled.sql
@@ -0,0 +1,7 @@
+select *
+from table(information_schema.query_history())
+order by start_time desc
+-- where query_id = '01c138f8-0002-9716-0002-e8ae000491fe'
+;
+
+drop view tasty_bytes_dbt_db.dev.raw_pos_franchise;
